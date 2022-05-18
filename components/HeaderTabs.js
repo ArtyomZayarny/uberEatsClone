776e1b7e54d3a7b1fc9042e006b67d807cb36fd9
ArtyomZayarny <artyom.zayarny@gmail.com>
2022-05-19 00:14:08 +0300

    Added state and styles for active tab

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 export default function HeaderTabs() {
+  const [activeTab, setActiveTab] = React.useState('Delivery');
   return (
-    <View>
-      <Text>Header Tabs</Text>
+    <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+      <HeaderButton
+        text="Delivery"
+        btnColor="black"
+        textColor="white"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+      <HeaderButton
+        text="Pickup"
+        btnColor="white"
+        textColor="black"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
     </View>
   );
 }
+const HeaderButton = (props) => (
+  <TouchableOpacity
+    style={{
+      paddingVertical: 6,
+      paddingHorizontal: 16,
+      borderRadius: 30,
+      backgroundColor: props.activeTab === props.text ? 'black' : 'white',
+    }}
+    onPress={() => props.setActiveTab(props.text)}
+  >
+    <Text
+      style={{
+        color: props.activeTab === props.text ? 'white' : 'black',
+        fontSize: 15,
+        fontWeight: '900',
+      }}
+    >
+      {props.text}
+    </Text>
+  </TouchableOpacity>
+);
