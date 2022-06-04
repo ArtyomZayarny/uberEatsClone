6d8e5f4d2c6e8105a0190c1922b96ccbce1f293c
Artem Zaiarnyi <artem_zaiarnyi@epam.com>
2022-06-04 16:35:14 +0300

    update eslint settings

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import RestrauntDetails from "./screens/RestrauntDetails";
+import React from 'react';
+import { RestrauntDetails } from './screens/RestrauntDetails';
 
 export default function App() {
   return <RestrauntDetails />;
