8973f432454136ac64ea4ff731267409bb49d703
Artem Zaiarnyi <artem_zaiarnyi@epam.com>
2022-06-18 07:39:46 +0300

    Added ViewCart button component

--- screens/RestrauntDetails.tsx
+++ screens/RestrauntDetails.tsx
@@ -3,9 +3,15 @@ import React from 'react';
 import { Divider } from 'react-native-elements';
 import { About } from '../components/restaurantDetails/About';
 import { MenuItems } from '../components/restaurantDetails/MenuItems';
+import { ViewCart } from '../components/restaurantDetails/ViewCart';
 
-export const RestaurantDetails: React.FC = ({ route }) => (
-  <View>
+export const RestaurantDetails: React.FC = ({ route, navigation }) => (
+  <View
+    style={{
+      position: 'relative',
+      height: '100%',
+    }}
+  >
     <About route={route} />
     <Divider
       width={1.8}
@@ -14,5 +20,6 @@ export const RestaurantDetails: React.FC = ({ route }) => (
       }}
     />
     <MenuItems />
+    <ViewCart navigation={navigation} restaurantName={route.params.name} />
   </View>
 );
